bc990b4caa266ee2db1f63f27e11bda2823f0032
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 13:16:37 -0300

    fixes

--- src/flext_target_oracle_oic/__init__.py
+++ src/flext_target_oracle_oic/__init__.py
@@ -18,18 +18,38 @@ from flext_target_oracle_oic.__version__ import (
 )
 
 if TYPE_CHECKING:
-    from flext_oracle_oic import d, e, h, r, s, x
+    from flext_oracle_oic import d as d, e as e, h as h, r as r, s as s, x as x
     from flext_target_oracle_oic.api import (
-        FlextTargetOracleOicService,
-        target_oracle_oic,
+        FlextTargetOracleOicService as FlextTargetOracleOicService,
+        target_oracle_oic as target_oracle_oic,
     )
-    from flext_target_oracle_oic.cli import FlextTargetOracleOicCli, main
-    from flext_target_oracle_oic.constants import FlextTargetOracleOicConstants, c
-    from flext_target_oracle_oic.models import FlextTargetOracleOicModels, m
-    from flext_target_oracle_oic.protocols import FlextTargetOracleOicProtocols, p
-    from flext_target_oracle_oic.settings import FlextTargetOracleOicSettings
-    from flext_target_oracle_oic.typings import FlextTargetOracleOicTypes, t
-    from flext_target_oracle_oic.utilities import FlextTargetOracleOicUtilities, u
+    from flext_target_oracle_oic.cli import (
+        FlextTargetOracleOicCli as FlextTargetOracleOicCli,
+        main as main,
+    )
+    from flext_target_oracle_oic.constants import (
+        FlextTargetOracleOicConstants as FlextTargetOracleOicConstants,
+        c as c,
+    )
+    from flext_target_oracle_oic.models import (
+        FlextTargetOracleOicModels as FlextTargetOracleOicModels,
+        m as m,
+    )
+    from flext_target_oracle_oic.protocols import (
+        FlextTargetOracleOicProtocols as FlextTargetOracleOicProtocols,
+        p as p,
+    )
+    from flext_target_oracle_oic.settings import (
+        FlextTargetOracleOicSettings as FlextTargetOracleOicSettings,
+    )
+    from flext_target_oracle_oic.typings import (
+        FlextTargetOracleOicTypes as FlextTargetOracleOicTypes,
+        t as t,
+    )
+    from flext_target_oracle_oic.utilities import (
+        FlextTargetOracleOicUtilities as FlextTargetOracleOicUtilities,
+        u as u,
+    )
 _LAZY_IMPORTS = build_lazy_import_map(
     {
         ".api": (
