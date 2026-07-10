aa664b675f32139764766338e619a12d49dedf0e
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-10 10:21:15 -0300

    fixes

--- tests/settings.py
+++ tests/settings.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from typing import Annotated
 
-from flext_tests.settings import FlextTestsSettings
+from flext_tests import FlextTestsSettings
 
 from flext_target_oracle_oic import FlextTargetOracleOicSettings, t, u
 
