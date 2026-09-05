5f684b6d70e4c6fecab210c77f3df36f515b8e30
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-05 15:49:16 -0300

    [WIP] refactor: checkpoint fleet conformance

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,18 +3,18 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from types import MappingProxyType
+from typing import TYPE_CHECKING
 
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from . import unit as unit
-    from flext_target_oracle_oic import FlextTargetOracleOicConstants
-    from flext_tests import FlextTestsConstants, d, e, h, r, td, tf, tk, tm, tv, x
     from typing import Final
 
+    from flext_target_oracle_oic import FlextTargetOracleOicConstants
+    from flext_tests import FlextTestsConstants, d, e, h, r, td, tf, tk, tm, tv, x
+
+    from . import unit as unit
     from .base import (
         TestsFlextTargetOracleOicServiceBase,
         TestsFlextTargetOracleOicServiceBase as s,
--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -3,49 +3,23 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from types import MappingProxyType
+from typing import TYPE_CHECKING
 
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
     from flext_tests import c, d, e, h, m, p, r, s, t, td, tf, tk, tm, tv, u, x
-
-    from .test_cli_entrypoint import TestsFlextTargetOracleOicCliEntrypoint
-    from .test_e2e import TestsFlextTargetOracleOicE2e, load_test_config, target
-    from .test_e2e_sinks import (
-        DummySingerTargetE2E,
-        TestsFlextTargetOracleOicE2eSinks,
-        singer_target,
-    )
-    from .test_module_governance import TestsFlextTargetOracleOicModuleGovernance
-    from .test_target import (
-        AuthTestSettings,
-        DummySingerTarget,
-        TestsFlextTargetOracleOicTarget,
-    )
 __all__: tuple[str, ...] = (
-    "AuthTestSettings",
-    "DummySingerTarget",
-    "DummySingerTargetE2E",
-    "TestsFlextTargetOracleOicCliEntrypoint",
-    "TestsFlextTargetOracleOicE2e",
-    "TestsFlextTargetOracleOicE2eSinks",
-    "TestsFlextTargetOracleOicModuleGovernance",
-    "TestsFlextTargetOracleOicTarget",
     "c",
     "d",
     "e",
     "h",
-    "load_test_config",
     "m",
     "p",
     "r",
     "s",
-    "singer_target",
     "t",
-    "target",
     "td",
     "tf",
     "tk",
@@ -58,19 +32,6 @@ __all__: tuple[str, ...] = (
 _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
-            ".test_cli_entrypoint": ("TestsFlextTargetOracleOicCliEntrypoint",),
-            ".test_e2e": ("TestsFlextTargetOracleOicE2e", "load_test_config", "target"),
-            ".test_e2e_sinks": (
-                "DummySingerTargetE2E",
-                "TestsFlextTargetOracleOicE2eSinks",
-                "singer_target",
-            ),
-            ".test_module_governance": ("TestsFlextTargetOracleOicModuleGovernance",),
-            ".test_target": (
-                "AuthTestSettings",
-                "DummySingerTarget",
-                "TestsFlextTargetOracleOicTarget",
-            ),
             "flext_tests": (
                 "c",
                 "d",
@@ -88,7 +49,7 @@ _LAZY_IMPORTS = MappingProxyType(
                 "tv",
                 "u",
                 "x",
-            ),
+            )
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
