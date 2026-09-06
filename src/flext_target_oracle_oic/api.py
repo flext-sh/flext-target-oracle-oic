be670f6fcae0c37108668e2b618a5a1af5dd2c11
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-05 23:27:56 -0300

    [WIP] checkpoint automated mod and formatting

--- src/flext_target_oracle_oic/api.py
+++ src/flext_target_oracle_oic/api.py
@@ -37,4 +37,5 @@ class FlextTargetOracleOicService(FlextMeltanoTargetServiceBase):
             stream_name=stream_name, schema=schema, target_config=target_config
         )
 
-__all__: list[str] = ["FlextTargetOracleOicService", ]
+
+__all__: list[str] = ["FlextTargetOracleOicService"]
