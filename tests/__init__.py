11985cd648df99ada0f8d6e38146708aaa1d7e33
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-08 00:53:23 -0300

    [WIP] wave-2 flext-target-oracle-oic: regenerate tests facade projection

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,8 +9,6 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from typing import Final
-
     from flext_target_oracle_oic import FlextTargetOracleOicConstants
     from flext_tests import FlextTestsConstants, d, e, h, r, td, tf, tk, tm, tv, x
 
@@ -41,7 +39,6 @@ if TYPE_CHECKING:
         TestsFlextTargetOracleOicUtilities as u,
     )
 __all__: tuple[str, ...] = (
-    "Final",
     "FlextTargetOracleOicConstants",
     "FlextTestsConstants",
     "TestsFlextTargetOracleOicConstants",
@@ -95,7 +92,6 @@ _LAZY_IMPORTS = MappingProxyType(
                 "tv",
                 "x",
             ),
-            "typing": ("Final",),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
