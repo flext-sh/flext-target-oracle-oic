9ad4076f0972c3beee9dfbf39d551d639026b4dd
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-22 20:35:42 -0300

    chore(gen): project ruff namespace-packages and rope-cured facets

--- src/flext_target_oracle_oic/_constants/__init__.py
+++ src/flext_target_oracle_oic/_constants/__init__.py
@@ -10,6 +10,8 @@ from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
     from .base import FlextTargetOracleOicConstantsBase
+
+
 __all__: tuple[str, ...] = ("FlextTargetOracleOicConstantsBase",)
 
 _LAZY_IMPORTS = MappingProxyType(
