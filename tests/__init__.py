c70d83c3a789e1e7a20242c594d660143f0df8ca
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-22 13:00:59 -0300

    merge: stabilize branch against 0.12.0-dev tip + converge gen

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,31 +9,9 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_auth import auth
-    from flext_cli import cli
-    from flext_infra import docs_main, infra
-    from flext_meltano import meltano
-    from flext_oracle_oic import oracle_oic
-    from flext_tests import (
-        active_rules,
-        api,
-        config,
-        discover_repository_root,
-        install_local_packages,
-        load_infra_report,
-        settings,
-        split_csv,
-        td,
-        tf,
-        tk,
-        tm,
-        tv,
-    )
-    from flext_web import web
-    from pydantic_core import from_json, to_json, to_jsonable_python
+    from flext_tests import FlextTestsConstants, d, e, h, r, td, tf, tk, tm, tv, x
 
-    from flext_core import core, d, e, h, lazy_attribute, r, x
-    from flext_target_oracle_oic import main, target_oracle_oic
+    from flext_target_oracle_oic import FlextTargetOracleOicConstants
 
     from . import unit
     from .base import (
@@ -62,6 +40,8 @@ if TYPE_CHECKING:
         TestsFlextTargetOracleOicUtilities as u,
     )
 __all__: tuple[str, ...] = (
+    "FlextTargetOracleOicConstants",
+    "FlextTestsConstants",
     "TestsFlextTargetOracleOicConstants",
     "TestsFlextTargetOracleOicModels",
     "TestsFlextTargetOracleOicProtocols",
@@ -69,44 +49,22 @@ __all__: tuple[str, ...] = (
     "TestsFlextTargetOracleOicSettings",
     "TestsFlextTargetOracleOicTypes",
     "TestsFlextTargetOracleOicUtilities",
-    "active_rules",
-    "api",
-    "auth",
     "c",
-    "cli",
-    "config",
-    "core",
     "d",
-    "discover_repository_root",
-    "docs_main",
     "e",
-    "from_json",
     "h",
-    "infra",
-    "install_local_packages",
-    "lazy_attribute",
-    "load_infra_report",
     "m",
-    "main",
-    "meltano",
-    "oracle_oic",
     "p",
     "r",
     "s",
-    "settings",
-    "split_csv",
     "t",
-    "target_oracle_oic",
     "td",
     "tf",
     "tk",
     "tm",
-    "to_json",
-    "to_jsonable_python",
     "tv",
     "u",
     "unit",
-    "web",
     "x",
 )
 
@@ -121,30 +79,20 @@ _LAZY_IMPORTS = MappingProxyType(
             ".typings": ("TestsFlextTargetOracleOicTypes", "t"),
             ".unit": ("unit",),
             ".utilities": ("TestsFlextTargetOracleOicUtilities", "u"),
-            "flext_auth": ("auth",),
-            "flext_cli": ("cli",),
-            "flext_core": ("core", "d", "e", "h", "lazy_attribute", "r", "x"),
-            "flext_infra": ("docs_main", "infra"),
-            "flext_meltano": ("meltano",),
-            "flext_oracle_oic": ("oracle_oic",),
-            "flext_target_oracle_oic": ("main", "target_oracle_oic"),
+            "flext_target_oracle_oic": ("FlextTargetOracleOicConstants",),
             "flext_tests": (
-                "active_rules",
-                "api",
-                "config",
-                "discover_repository_root",
-                "install_local_packages",
-                "load_infra_report",
-                "settings",
-                "split_csv",
+                "FlextTestsConstants",
+                "d",
+                "e",
+                "h",
+                "r",
                 "td",
                 "tf",
                 "tk",
                 "tm",
                 "tv",
+                "x",
             ),
-            "flext_web": ("web",),
-            "pydantic_core": ("from_json", "to_json", "to_jsonable_python"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
